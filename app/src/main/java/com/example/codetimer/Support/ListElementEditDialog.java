597b68cad9a759281ad85bd6827696b51e864ce9
Jonas <jonas.guenster@gmx.de>
2019-10-25 00:06:33 +0200

    Fixed editDialog, made it look nice

--- app/src/main/java/com/example/codetimer/Support/ListElementEditDialog.java
+++ app/src/main/java/com/example/codetimer/Support/ListElementEditDialog.java
@@ -7,6 +7,8 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.NumberPicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,8 +17,14 @@ import androidx.fragment.app.DialogFragment;
 
 import com.example.codetimer.R;
 
-public class ListElementEditDialog extends AppCompatDialogFragment {
+import java.text.Normalizer;
 
+public class ListElementEditDialog extends AppCompatDialogFragment {
+    private NumberPicker hour_picker;
+    private NumberPicker seconds_picker;
+    public EditText edit_name;
+    public int hour;
+    public int seconds;
 
     public interface  ListElementEditDialogListener{
         void onDialogPositiveClick(DialogFragment dialog);
@@ -40,6 +48,43 @@ public class ListElementEditDialog extends AppCompatDialogFragment {
         LayoutInflater inflater = requireActivity().getLayoutInflater();
 
         View v = inflater.inflate(R.layout.edit_element_dialog, null);
+        hour_picker = v.findViewById(R.id.hour_picker);
+        seconds_picker = v.findViewById(R.id.seconds_picker);
+        edit_name = v.findViewById(R.id.edit_name);
+
+        hour_picker.setMaxValue(60);
+        hour_picker.setMinValue(0);
+        hour_picker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                hour = newVal;
+            }
+        });
+        hour_picker.setFormatter(new NumberPicker.Formatter() {
+            @Override
+            public String format(int value) {
+                return String.format("%02d", value);
+            }
+        });
+
+        seconds_picker.setMaxValue(60);
+        seconds_picker.setMinValue(0);
+        seconds_picker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                seconds = newVal;
+            }
+        });
+        seconds_picker.setFormatter(new NumberPicker.Formatter() {
+            @Override
+            public String format(int value) {
+                return String.format("%02d", value);
+            }
+        });
+
+
+
+
         builder.setView(v);
         builder.setNegativeButton(R.string.edit_dialog_negativ, new DialogInterface.OnClickListener() {
             @Override
